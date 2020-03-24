f33b3add0c4931266b135d14884f074caef9c18d
az <az@ua-azhuravok.route66>
2020-03-24 22:42:41 +0200

    Overloaded arithmetic operators for fractions

--- mathmodule_pkg/fractions/fractionClass.py
+++ mathmodule_pkg/fractions/fractionClass.py
@@ -1,4 +1,4 @@
-from ..number_theory.GCDandLCM import gcd
+from ..number_theory.GCDandLCM import gcd, lcm
 
 class fraction:
     def __init__(self, n, d):
@@ -16,3 +16,68 @@ class fraction:
     
     def getDecimal(self):
         return self.n/self.d
+    
+    def __add__(self, other):
+        r = fraction(self.n * other.d + other.n * self.d, self.d*other.d)
+        r.simplify()
+        return r
+    
+    def __sub__(self, other):
+        r = fraction(self.n * other.d - other.n * self.d, self.d*other.d)
+        r.simplify()
+        return r
+    
+    def __mul__(self, other):
+        r = fraction(self.n * other.n, self.d * other.d)
+        r.simplify()
+        return r
+
+    def __truediv__(self, other):
+        r = fraction(self.n * other.d, self.d * other.n)
+        r.simplify()
+        return r
+
+    def __eq__(self, other):
+        selfs = self
+        selfs.simplify()
+        others = other
+        others.simplify()
+        return selfs.n == others.n and selfs.d == others.d
+
+    def __ne__(self, other):
+        selfs = self
+        selfs.simplify()
+        others = other
+        others.simplify()
+        return selfs.n != others.n or selfs.d != others.d
+    
+    def __lt__(self, other):
+        selfs = self
+        selfs.simplify()
+        others = other
+        others.simplify()
+        return selfs.n * others.d < others.n * selfs.d
+    
+    def __le__(self, other):
+        return self < other or self == other
+    
+    def __gt__(self, other):
+        return not(self <= other)
+    
+    def __ge__(self, other):
+        return self > other or self == other
+    
+    def __iadd__(self, other):
+        self = self + other
+    
+    def __isub__(self, other):
+        self = self - other
+    
+    def __imul__(self, other):
+        self = self * other
+    
+    def __idiv__(self, other):
+        self = self / other
+    
+    def __str__(self):
+        return self.n + "/" + self.d
