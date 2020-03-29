8ea2788a5bd5c54ef6920cfadc3464625548f1ff
az <az@ua-azhuravok.route66>
2020-03-29 14:27:28 +0300

    Created more tests for averages

--- avg_test.py
+++ avg_test.py
@@ -2,3 +2,12 @@ from mathmodule_pkg import *
 
 def test_am():
     assert am([1, 2]) == fraction(3, 2)
+
+def test_gm():
+    assert gm([1, 2]) == 2**(1/2)
+
+def test_hm():
+    assert hm([1, 2]) == 4/3
+
+def test_median():
+    assert median([1, 2, 3]) == 2
